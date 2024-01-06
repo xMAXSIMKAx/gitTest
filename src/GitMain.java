0d1dc0337d2eb6316101b4bdc97e3b241503f410
xMAXSIMKAx <maxymram@gmail.com>
2024-01-06 18:57:01 +0100

    Initial commit

---
+++ src/GitMain.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GitMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("num 1:");
+        double a = scanner.nextDouble();
+
+        System.out.println("num 2:");
+        double b = scanner.nextDouble();
+
+        System.out.println("Res = " + a + b);
+
+    }
+
+
+}
