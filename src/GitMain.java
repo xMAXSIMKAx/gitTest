43e693a3fd4adbfa98d43af4f65b933ca69fafbd
xMAXSIMKAx <maxymram@gmail.com>
2024-01-06 19:05:05 +0100

    update add functional

--- src/GitMain.java
+++ src/GitMain.java
@@ -4,12 +4,17 @@ public class GitMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("num 1:");
-        double a = scanner.nextDouble();
+        double num1 = scanner.nextDouble();
 
         System.out.println("num 2:");
-        double b = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
 
-        System.out.println("Res = " + a + b);
+        System.out.println(add(num1, num2));
+
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
 
     }
 
