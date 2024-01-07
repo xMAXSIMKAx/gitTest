41c1d6c5b07eb8fa8c9a2472f696c9b1d8236110
xMAXSIMKAx <maxymram@gmail.com>
2024-01-07 15:17:19 +0100

    add try catch

--- src/GitMain.java
+++ src/GitMain.java
@@ -1,29 +1,35 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GitMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("num 1:");
-        double num1 = scanner.nextDouble();
+        try {
+            System.out.println("num 1:");
 
-        System.out.println("num 2:");
-        double num2 = scanner.nextDouble();
-        System.out.println("виберіть дію додавання 1 чи віднімання 2");
-        int action = scanner.nextInt();
+            double num1 = scanner.nextDouble();
 
-        switch ( action){
-            case 1:{
-                System.out.println(add(num1,num2));
+            System.out.println("num 2:");
+            double num2 = scanner.nextDouble();
+            System.out.println("виберіть дію додавання 1 чи віднімання 2");
+            int action = scanner.nextInt();
 
+            switch (action) {
+                case 1: {
+                    System.out.println(add(num1, num2));
+
+                }
+                break;
+                case 2: {
+                    System.out.println(subtraction(num1, num2));
+
+                }
+                break;
+                default:
+                    System.out.println("error try again");
             }
-            break;
-            case 2:{
-                System.out.println(subtraction(num1,num2));
-
-            }
-            break;
-            default:
-                System.out.println("error try again");
+        } catch (InputMismatchException e) {
+            System.out.println("error try again");
         }
 
     }
@@ -32,7 +38,8 @@ public class GitMain {
         return a + b;
 
     }
-    public static double subtraction(double a,double b){
+
+    public static double subtraction(double a, double b) {
         return a - b;
     }
 
